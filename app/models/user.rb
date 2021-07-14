cdb77488dc0a889db6f26cc4430fecb7022cb4a1
koluhei <qpal79@gmail.com>
2021-07-14 19:05:29 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name, presence:true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :l_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :f_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
 end
