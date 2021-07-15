c80d733c0b0da5e361e93c44d4a88e9ca7f0ef2c
koluhei <qpal79@gmail.com>
2021-07-15 17:22:45 +0900

    商品出品画面への遷移可能に

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
     validates :delivery_charge_id
     validates :prefecture_id
     validates :days_to_ship_id
-    validates :price, numericality: { greater_than_or_equal_to 300, less_than_or_equal_to 9999999999, only_integer: true }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999999, only_integer: true }
   end
 end
