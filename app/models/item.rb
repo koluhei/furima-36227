e7ced2790f4d0e41709e6098558927d3041b5666
koluhei <qpal79@gmail.com>
2021-07-15 17:09:55 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :price, numericality: { greater_than_or_equal_to 300, less_than_or_equal_to 9999999999, only_integer: true }
+  end
 end
