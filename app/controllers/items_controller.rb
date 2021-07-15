eda1c0b3ad8e60fefd8596b8443abdf120ca6ccb
koluhei <qpal79@gmail.com>
2021-07-15 19:51:31 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,21 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @items = @item
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_charge_id, :days_to_ship_id, :prefecture_id, :price).merge(user_id: current_user.id)
     
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
-    with_options numericality {other_than: 1} do
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :status_id
       validates :delivery_charge_id
@@ -12,7 +12,9 @@ class Item < ApplicationRecord
     end
   end
 
-
+  has_one_attached :image
+  belongs_to :user
+    
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :prefecture
