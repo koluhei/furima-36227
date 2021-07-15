6d63d4c365e3befa2504c203506d0c74f25258e9
koluhei <qpal79@gmail.com>
2021-07-15 18:32:16 +0900

    ActiveHashおよびその関連の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,21 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :status_id
-    validates :delivery_charge_id
-    validates :prefecture_id
-    validates :days_to_ship_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999999, only_integer: true }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
+    with_options numericality {other_than: 1} do
+      validates :category_id
+      validates :status_id
+      validates :delivery_charge_id
+      validates :prefecture_id
+      validates :days_to_ship_id
+    end
   end
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :days_to_ship
 end
