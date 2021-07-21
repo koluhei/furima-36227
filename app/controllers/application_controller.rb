8c0d8fa0482a8c6e11a1e392975753f47ac62c17
koluhei <qpal79@gmail.com>
2021-07-21 10:48:37 +0900

    Basic認証と環境変数の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permited_parameters, if: :devise_controller?
   
 
   
   
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :l_kana, :f_kana, :birthday])
